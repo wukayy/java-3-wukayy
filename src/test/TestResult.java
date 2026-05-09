c11f9023e23b042799c998b369acab567ccc06c8
bakulindaniil <168584838+wukayy@users.noreply.github.com>
2026-05-09 15:04:30 +0300

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,26 @@
 package test;
 
 import domain.Exercise;
+import java.util.Scanner;
 
 public class TestResult {
 
     public static void main(String[] args) {
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+
+        int number = scanner.nextInt();
+
+        Exercise exercise = new Exercise();
+
+        boolean result = exercise.Calculate(number);
+
+        if (result) {
+            System.out.println(number + " is an Armstrong number");
+        } else {
+            System.out.println(number + " is NOT an Armstrong number");
+        }
     }
 }
